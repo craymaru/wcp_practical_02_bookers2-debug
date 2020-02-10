c32866da1e0f5b51874cd0f556e0e3821d0566ca
Cray <craymaru@gmail.com>
2020-02-10 18:50:21 +0900

    Fixed: Incorrect Syntaxes, Routes, Actions, Models
    * Fixed missing end tag in "routes.rb".
    * Fixed missing end tag in "users_controller.rb".
    * Fixed missing bootstrap-sass gem.
    * Fixed incorrect priority of users and devise/users in "route.rb".
    * Fixed incorrect field in "devise/registrations/new.html.erb".
    * Fixed incorrect tag in "application.html.erb".
    * Fixed missing yield in "application.html.erb".
    * Fixed incorrect associations in book and user models.
    * Fixed two wrong paths in top.html.erb
    * Fixed wrong variable in show.html.erb
    * Fixed wrong strong params with create action in books controller.
    * Fixed save not linked to books controller.
    * Fixed wrong action name "delete" to "Destroy" in books.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  root "home#top"
+  get "home/about"
   devise_for :users
-  root 'home#top'
-  get 'home/about'
+  resources :users, only: [:show, :index, :edit, :update, :destroy]
+  resources :books
+end
